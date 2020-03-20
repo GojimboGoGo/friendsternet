9a9ad1760a8ab3bb376f4542897f12033e41573b
Joseph <joseph.miclatbourne@gmail.com>
2020-03-20 19:21:04 +0800

    Initial commit

---
+++ src/main/java/jbourne/demo/friendsternet/FriendsternetApplication.java
@@ -0,0 +1,13 @@
+package jbourne.demo.friendsternet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsternetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsternetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jbourne/demo/friendsternet/FriendsternetApplicationTests.java
@@ -0,0 +1,13 @@
+package jbourne.demo.friendsternet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsternetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
